7110cca3bbf51b20ac63ddd718f735542c617735
mohamed fata <medfata3@gmail.com>
2020-11-04 18:57:08 +0100

    app.js first code

---
+++ clientSide/app.js
@@ -0,0 +1,56 @@
+myfunc = function(){
+    const xhr  = new XMLHttpRequest();
+
+
+
+    xhr.onreadystatechange = ()  => {
+        let listUl = document.querySelector('.list_section ul');
+        if(xhr.readyState == 4){
+
+            JSON.parse(xhr.response).forEach(list =>{
+  
+                let li = document.createElement("li");
+                li.textContent = `- ${list.name}`;
+                li.id = list.id;
+                listUl.appendChild(li);
+
+                li.addEventListener('click', function(){
+                    let list = document.querySelector(".item_section ul");
+                    list.innerHTML = "";
+                    getItems(this.id);
+                });
+            
+            });
+        }
+            
+    }
+
+    xhr.open("GET", "http://localhost:5000/lists",true);
+    xhr.send();
+}
+myfunc();
+
+getItems = function(id){
+ 
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = ()  => {
+        let itemUl = document.querySelector('.item_section ul');
+        if(xhr.readyState == 4){
+
+            JSON.parse(xhr.response).forEach(item =>{
+                let done  = (item.done) ? "done":"not yet";
+                let li = document.createElement("li");
+                li.textContent = `- ${item.value} - state :${done}`;
+  
+                itemUl.appendChild(li);
+       
+            
+            });
+        }
+     
+            
+    }
+
+    xhr.open("GET", `http://localhost:5000/listItems/${id}`,true);
+    xhr.send();
+}
