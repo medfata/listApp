f1deba9fe333b8a2b2dbaaa86fa88f59626d0436
mohamed fatta <medfata3@gmail.com>
2020-11-05 11:33:28 +0000

    frontEnd list section styles

--- clientSide/app.js
+++ clientSide/app.js
@@ -11,9 +11,14 @@ myfunc = function(){
             JSON.parse(xhr.response).forEach(list =>{
   
                 let li = document.createElement("li");
-                li.textContent = `- ${list.name}`;
+            
                 li.id = list.id;
-                listUl.appendChild(li);
+                li.className  =" btn-outline-info border border-info";
+                li.innerHTML =`<span>${list.name}</span><svg style="float:right;position: relative;right:5px;top: 9px;" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+              </svg>`;
+              
+              listUl.appendChild(li);
 
                 li.addEventListener('click', function(){
                     let list = document.querySelector(".item_section ul");
